12eb2f6892af313f42e15a8ed5c2ad580a6bb21b
gnanamoorthy-g <ggnanamoorthy215@gmail.com>
2024-08-23 00:15:31 +0530

    cors policy set on server

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ server.listen(port, () => {
     console.log("app running on port ::",port);
 });
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 var connections = [];
 
 io.on("connection", (socket) => {
